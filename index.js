e0cb9b4c22745ae12a41445894ebb4d16af65412
Phathompong <maipha1230@gmail.com>
2022-12-20 23:19:39 +0700

    update build

--- index.js
+++ index.js
@@ -11,14 +11,22 @@ app.use(express.urlencoded({ extended: true }))
 //use express static folder
 app.use('/api', express.static(path.join(__dirname, '/public')))
 
+// Set the path to the dist folder
+const distPath = path.join(__dirname, '/dist/booking-meeting-room');
+app.use(express.static(distPath));
+
 //database
 const db = require("./model/index.model");
 // db.sequelize.sync({ force: true, alter: true });
 db.sequelize.sync();
 
-app.get('/', (req, res) => {
-    return res.send('Welcome to api')
-})
+// app.get('/', (req, res) => {
+//     return res.send('Welcome to api')
+// })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+  });
 
 app.use('/api', require('./routes/index.route'))
 
